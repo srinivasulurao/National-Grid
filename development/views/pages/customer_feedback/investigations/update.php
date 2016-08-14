70a2f23b1f0e8ec2916f066ea5686ad9c8b373ae
nra <nra>
2016-08-15 04:53:28 +0530

    My Eyes are opened

---
+++ development/views/pages/customer_feedback/investigations/update.php
@@ -0,0 +1,42 @@
+<rn:meta title="Investigation of Complaints" template="standard.php" clickstream="list" login_required="true" //>
+
+<div class="rn_Hero">
+    <div class="rn_HeroInner">
+        <div class="rn_HeroCopy">
+		<h1>Complaint Investigations</h1>
+           <!-- <h1>#rn:msg:SUBMIT_QUESTION_OUR_SUPPORT_TEAM_CMD#</h1>
+            <p>#rn:msg:OUR_DEDICATED_RESPOND_WITHIN_48_HOURS_MSG#</p>!-->
+        </div>
+        
+        <br><br>      
+    </div>
+</div>
+
+<div class="rn_PageContent rn_AskQuestion rn_Container investigations">
+	<a href='/app/customer_feedback/investigations/list' style='float:right' class='link_button'>Back</a>
+	<h1>Complaint Details</h1>
+    <div class='collapsible-div investigation-playstation-1'><rn:widget path="custom/investigations/ComplaintDetails" /></div>
+        
+    <h1>Corrective Actions <span id='total_corrective_actions'></span></h1>     
+    <div class='collapsible-div investigation-playstation-2'><rn:widget path="custom/investigations/CorrectiveActions" /></div>
+  
+    <h1>Thread</h1>     
+    <div class='collapsible-div investigation-playstation-2'><rn:widget path="custom/investigations/UpdateInvestigation" /></div>
+    
+</div>
+
+<style>
+	.investigations h1{
+		    background: #40526B;
+    color: white;
+    padding: 10px;
+    margin: 20px 0 20px 0;
+    display: block !important;
+    width: 100%;
+    font-size:20px;
+    border-radius: 4px;
+    font-weight: normal;
+    box-shadow: 0px 0 10px 0px black;
+    width:100%;
+	}
+</style>
---
+++ development/widgets/custom/investigations/UpdateInvestigation/1.0/view.php
@@ -0,0 +1,11 @@
+<div id="rn_<?= $this->instanceID ?>" class="<?= $this->classList ?> inv-div">
+<a href="javascript:void(0)" id="add_thread" class="btn info_button"> + Add</a>
+<form action="/cc/CustomerFeedbackSystem/SaveThreadSendMail/i_id/<?php echo getUrlParm('i_id'); ?>" method="post" id="thread_submit" style='display:none'>
+	<div id='rn_ErrorLocation2'></div>	
+<rn:widget path="input/TextInput"  name="Incident.Threads" default_value="" label_input="Thread"  required="true"/>
+<rn:widget path="input/FormInput"  name='contacts.email' default_value="" label_input="Request From" default_value='' required="true"/>
+<span id='contact_look_up' style='position:relative;bottom:20px;'></span>
+<rn:widget path="input/FormSubmit" label_button="Submit " on_success_url='/app/customer_feedback/investigations/update' error_location="rn_ErrorLocation2" label_confirm_dialog="Thread Saved Successfully, Further a mail has been sent to forward & track the incident !"  />
+</form>
+<div id='thread_display' style='font-size:12px'><rn:widget path="output/IncidentThreadDisplay" label='Communication History' name="Incident.Threads" /></div>
+</div>
